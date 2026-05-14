8205729e3632fb6da1b4d884e7e59c70b597fd09
Laurent Cheylus <foxy@free.fr>
2026-05-14 17:50:39 +0200

    Modify usage for optional config file option
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- config.go
+++ config.go
@@ -30,7 +30,7 @@ type Config struct {
 
 // Print usage / help message
 func printUsage() {
-	fmt.Printf("Usage: %s [-h] [-v] [-d] [-f logfile] -c conffile | [-l address] [-p port] -s server [-P port]\n\n", version.Name)
+	fmt.Printf("Usage: %s [-h] [-v] [-d] [-f logfile] [-c conffile] | [-l address] [-p port] -s server [-P port]\n\n", version.Name)
 	fmt.Printf("Proxy to connect an IRC client to an ICB server\n")
 	fmt.Println("\nOptions:")
 	fmt.Println("  -h, --help\t\t\tShow this help message")
